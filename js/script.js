7123a8d1f6b9b8613ac707473c593e69b47b6d62
Nekrasov Vadim <n2v@list.ru>
2020-11-05 10:50:19 +0300

    refactor: Добавил анимацию на модальное окно

--- js/script.js
+++ js/script.js
@@ -90,6 +90,34 @@ const togglePopUp = () =>{
 };
 togglePopUp();
 
+//Анимация popup
+const animatePopup = () =>{
+  const popupContent = document.querySelector('.popup-content'),
+        popupBtn = document.querySelectorAll('.popup-btn');
+        
+        function animate( draw, duration) { // передает в функцую 2 параметра
+          let start = performance.now(); //начало загрузки страницы
+          requestAnimationFrame(function animate(time) {//time с начало вызова функции 
+            let timePassed = time - start;//время которое прошло с начало страницы
+            if (timePassed > duration || screen.width < 768) timePassed = duration;//время больше чем длительность анимации, то анимация прекращается 
+            draw(timePassed); // отрисовать её
+            if (timePassed < duration) {
+              requestAnimationFrame(animate);//если меньше то рисуется анимация
+            }
+          });
+        };
+
+        popupBtn.forEach((elem) =>{
+          elem.addEventListener('click', () =>{
+            animate(function(timePassed){
+              popupContent.style.left = timePassed / 50 + '%';
+            },2000);
+          });
+        });
+};
+animatePopup();
+
+
 //Скролл
 const scroll = () =>{
   const anchors = document.querySelectorAll('a[href*="#"]')//собираем все ссылки
@@ -109,6 +137,8 @@ const scroll = () =>{
   });
 };
 scroll();
+
+
 
 }); 
 
