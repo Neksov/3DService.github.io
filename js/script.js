6efdd7f45eb1d61ff80016e8c655c1ae99f3447c
Nekrasov Vadim <n2v@list.ru>
2020-11-12 23:48:13 +0300

    feat: реализовали работу калькулятора

--- js/script.js
+++ js/script.js
@@ -1,48 +1,48 @@
 window.addEventListener('DOMContentLoaded' , () =>{//load дожидается загрузки всей страницы, DOMContentLoaded дожидает только загрузки DOM дерево
   'use strict';
 //Tаймер 
-function countTimer(deadline){
-  let timerHours = document.querySelector('#timer-hours'),
-      timerMinutes = document.querySelector('#timer-minutes'),
-      timerSeconds = document.querySelector('#timer-seconds');
+// function countTimer(deadline){
+//   let timerHours = document.querySelector('#timer-hours'),
+//       timerMinutes = document.querySelector('#timer-minutes'),
+//       timerSeconds = document.querySelector('#timer-seconds');
       
-  function getTimeRemaining() { 
-    let dateStop = new Date(deadline).getTime(),//создаем экземпляр класса Date, заносим дедлайн,миллисекунды от этих дат
-      dateNow = new Date().getTime(),//создаем экземпляр класса Date, получаем текущую дату,миллисекунды от этих дат
-      timeRemaining = (dateStop - dateNow) / 1000, //получаем разницу между датами(кол-во милсек) и делим на 1000 получая секунды
-      seconds = ('0' + Math.floor(timeRemaining % 60)).slice(-2),// округляем и берем остаток от деления минуты, не выходя за рамки минуты 59-60 и 0 и тд
-      minutes = ('0' + Math.floor((timeRemaining / 60) % 60)).slice(-2),// округляем и делим на 60 получая минуты и берем остаток от деления для получения минут не выходя за рамки минуты 59-60 и 0 и тд 
-      hours = ('0' + Math.floor(timeRemaining / 60 / 60) % 24).slice(-2), // делим на кол секунд  и минут и получаем количество часов и берем остаток от деления для определения дней
-      day = Math.floor(timeRemaining / 60 / 60/ 24);//вывод дня 
+//   function getTimeRemaining() { 
+//     let dateStop = new Date(deadline).getTime(),//создаем экземпляр класса Date, заносим дедлайн,миллисекунды от этих дат
+//       dateNow = new Date().getTime(),//создаем экземпляр класса Date, получаем текущую дату,миллисекунды от этих дат
+//       timeRemaining = (dateStop - dateNow) / 1000, //получаем разницу между датами(кол-во милсек) и делим на 1000 получая секунды
+//       seconds = ('0' + Math.floor(timeRemaining % 60)).slice(-2),// округляем и берем остаток от деления минуты, не выходя за рамки минуты 59-60 и 0 и тд
+//       minutes = ('0' + Math.floor((timeRemaining / 60) % 60)).slice(-2),// округляем и делим на 60 получая минуты и берем остаток от деления для получения минут не выходя за рамки минуты 59-60 и 0 и тд 
+//       hours = ('0' + Math.floor(timeRemaining / 60 / 60) % 24).slice(-2), // делим на кол секунд  и минут и получаем количество часов и берем остаток от деления для определения дней
+//       day = Math.floor(timeRemaining / 60 / 60/ 24);//вывод дня 
 
-      return {//возвращаем значения 
-        hours,//свойства 
-        minutes,
-        seconds,
-        timeRemaining
-      };
-  };
+//       return {//возвращаем значения 
+//         hours,//свойства 
+//         minutes,
+//         seconds,
+//         timeRemaining
+//       };
+//   };
 
-  function updateClock(){//обновляем время динамически через setInterval
-    let timer = getTimeRemaining();
+//   function updateClock(){//обновляем время динамически через setInterval
+//     let timer = getTimeRemaining();
 
-    //выводим на экран
-    timerHours.textContent = timer.hours;
-    timerMinutes.textContent = timer.minutes;
-    timerSeconds.textContent = timer.seconds;
+//     //выводим на экран
+//     timerHours.textContent = timer.hours;
+//     timerMinutes.textContent = timer.minutes;
+//     timerSeconds.textContent = timer.seconds;
 
-    if(timer.timeRemaining > 0){
-      setInterval(updateClock, 1000);
-    }else if(timer.timeRemaining <= 0){
-      timerHours.textContent = '00';
-      timerMinutes.textContent = '00';
-      timerSeconds.textContent = '00';
-    }
+//     if(timer.timeRemaining > 0){
+//       setInterval(updateClock, 1000);
+//     }else if(timer.timeRemaining <= 0){
+//       timerHours.textContent = '00';
+//       timerMinutes.textContent = '00';
+//       timerSeconds.textContent = '00';
+//     }
 
-  };
-  updateClock();
-};
-countTimer('30 November 2020');
+//   };
+//   updateClock();
+// };
+// countTimer('30 November 2020');
 
 //меню, откртие/закрытие 
 const toggleMenu = ()=>{
@@ -311,15 +311,58 @@ const imgChange = () =>{
 imgChange();
 
 //калькулятор
-const сalc = () =>{
-  const itemCalc = document.querySelector('.calc-block');
-  itemCalc.addEventListener('input', (e) =>{
+const сalc = (price = 100) =>{
+  const calcBlock = document.querySelector('.calc-block'),
+        calcType = document.querySelector('.calc-type'),
+        calcSquare = document.querySelector('.calc-square'),
+        calcCount = document.querySelector('.calc-count'),
+        calcDay = document.querySelector('.calc-day'),
+        totalValue = document.getElementById('total'); 
+
+  //проверка вводимых дынных-ТОЛЬКО ЦИФРЫ
+  calcBlock.addEventListener('input', (e) =>{
     let target = e.target;
     if(target.matches('.calc-square') || target.matches('.calc-count') || target.matches('.calc-day')){
       target.value = target.value.replace(/\D/g, ''); // ограничиваем ввод всего кроме цифр
     }
   });
-};
-сalc();
-}); 
 
+  //реализация расчетов калькулятора
+  const countSum = () =>{
+    let total = 0,
+    countValue = 1,
+    dayValue = 1;
+
+    const typeValue = calcType.options[calcType.selectedIndex].value,//получаем наше value 
+          squareValue = +calcSquare.value;
+
+    if(calcCount.value > 1){ //количество помещений 
+      countValue += (calcCount.value - 1) / 10; //получаем десятую долю и ее прибавляем
+    }
+
+    if(calcDay.value && calcDay.value < 5){//количество дней  
+      dayValue *= 2;
+    }else if(calcDay.value && calcDay.value < 10){
+      dayValue *= 1.5;
+    }
+
+    if(typeValue && squareValue){// проверка и умножение введеных данных площадь и обьект
+      total = price * typeValue * squareValue * countValue * dayValue;
+    }
+
+    totalValue.textContent = total; //вывод итоговая цена
+  };
+  
+  calcBlock.addEventListener('change', (e) =>{
+    let target = e.target;
+    if(target === calcType || target === calcSquare || target === calcCount || target === calcDay ){
+      countSum();
+
+    }
+  });
+
+
+};
+сalc(100);//при вызове калькулятора передаем сразу цену
+
+}); 
