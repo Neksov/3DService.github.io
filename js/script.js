e08796ad5c8806d368deacae397a23be8a6dbec7
Nekrasov Vadim <n2v@list.ru>
2020-11-15 12:04:03 +0300

    feat: Добавили проверку ввода в инпуты у форм

--- js/script.js
+++ js/script.js
@@ -1,48 +1,48 @@
 window.addEventListener('DOMContentLoaded' , () =>{//load дожидается загрузки всей страницы, DOMContentLoaded дожидает только загрузки DOM дерево
   'use strict';
 //Tаймер 
-// function countTimer(deadline){
-//   let timerHours = document.querySelector('#timer-hours'),
-//       timerMinutes = document.querySelector('#timer-minutes'),
-//       timerSeconds = document.querySelector('#timer-seconds');
+function countTimer(deadline){
+  let timerHours = document.querySelector('#timer-hours'),
+      timerMinutes = document.querySelector('#timer-minutes'),
+      timerSeconds = document.querySelector('#timer-seconds');
       
-//   function getTimeRemaining() { 
-//     let dateStop = new Date(deadline).getTime(),//создаем экземпляр класса Date, заносим дедлайн,миллисекунды от этих дат
-//       dateNow = new Date().getTime(),//создаем экземпляр класса Date, получаем текущую дату,миллисекунды от этих дат
-//       timeRemaining = (dateStop - dateNow) / 1000, //получаем разницу между датами(кол-во милсек) и делим на 1000 получая секунды
-//       seconds = ('0' + Math.floor(timeRemaining % 60)).slice(-2),// округляем и берем остаток от деления минуты, не выходя за рамки минуты 59-60 и 0 и тд
-//       minutes = ('0' + Math.floor((timeRemaining / 60) % 60)).slice(-2),// округляем и делим на 60 получая минуты и берем остаток от деления для получения минут не выходя за рамки минуты 59-60 и 0 и тд 
-//       hours = ('0' + Math.floor(timeRemaining / 60 / 60) % 24).slice(-2), // делим на кол секунд  и минут и получаем количество часов и берем остаток от деления для определения дней
-//       day = Math.floor(timeRemaining / 60 / 60/ 24);//вывод дня 
+  function getTimeRemaining() { 
+    let dateStop = new Date(deadline).getTime(),//создаем экземпляр класса Date, заносим дедлайн,миллисекунды от этих дат
+      dateNow = new Date().getTime(),//создаем экземпляр класса Date, получаем текущую дату,миллисекунды от этих дат
+      timeRemaining = (dateStop - dateNow) / 1000, //получаем разницу между датами(кол-во милсек) и делим на 1000 получая секунды
+      seconds = ('0' + Math.floor(timeRemaining % 60)).slice(-2),// округляем и берем остаток от деления минуты, не выходя за рамки минуты 59-60 и 0 и тд
+      minutes = ('0' + Math.floor((timeRemaining / 60) % 60)).slice(-2),// округляем и делим на 60 получая минуты и берем остаток от деления для получения минут не выходя за рамки минуты 59-60 и 0 и тд 
+      hours = ('0' + Math.floor(timeRemaining / 60 / 60) % 24).slice(-2), // делим на кол секунд  и минут и получаем количество часов и берем остаток от деления для определения дней
+      day = Math.floor(timeRemaining / 60 / 60/ 24);//вывод дня 
 
-//       return {//возвращаем значения 
-//         hours,//свойства 
-//         minutes,
-//         seconds,
-//         timeRemaining
-//       };
-//   };
+      return {//возвращаем значения 
+        hours,//свойства 
+        minutes,
+        seconds,
+        timeRemaining
+      };
+  };
 
-//   function updateClock(){//обновляем время динамически через setInterval
-//     let timer = getTimeRemaining();
+  function updateClock(){//обновляем время динамически через setInterval
+    let timer = getTimeRemaining();
 
-//     //выводим на экран
-//     timerHours.textContent = timer.hours;
-//     timerMinutes.textContent = timer.minutes;
-//     timerSeconds.textContent = timer.seconds;
+    //выводим на экран
+    timerHours.textContent = timer.hours;
+    timerMinutes.textContent = timer.minutes;
+    timerSeconds.textContent = timer.seconds;
 
-//     if(timer.timeRemaining > 0){
-//       setInterval(updateClock, 1000);
-//     }else if(timer.timeRemaining <= 0){
-//       timerHours.textContent = '00';
-//       timerMinutes.textContent = '00';
-//       timerSeconds.textContent = '00';
-//     }
+    if(timer.timeRemaining > 0){
+      setInterval(updateClock, 1000);
+    }else if(timer.timeRemaining <= 0){
+      timerHours.textContent = '00';
+      timerMinutes.textContent = '00';
+      timerSeconds.textContent = '00';
+    }
 
-//   };
-//   updateClock();
-// };
-// countTimer('30 November 2020');
+  };
+  updateClock();
+};
+countTimer('30 November 2020');
 
 //меню, откртие/закрытие 
 const toggleMenu = ()=>{
@@ -374,32 +374,39 @@ const sendForm = () =>{
   const form1 = document.getElementById('form1'),// форм главная
         form2 = document.getElementById('form2'),// форма вопросы 
         form3 = document.getElementById('form3'),// форма модалка 
-        formPhone = document.querySelectorAll('input[type=tel]'),
-        formText = document.querySelectorAll('input[type=text]'),
-        topForm = document.querySelector('.top-form');
-       
+        input = document.querySelectorAll('input'),
+        topForm = document.querySelector('.top-form'),
+        mess = document.querySelector('.mess'),
+        formName = document.getElementById('form1-name'),
+        formEmail = document.getElementById('form1-email'),
+        formTel = document.getElementById('form1-phone'),
+
+        formName2 = document.getElementById('form2-name'),
+        formEmail2 = document.getElementById('form2-email'),
+        formTel2 = document.getElementById('form2-phone'),
+
+        formName3 = document.getElementById('form3-name'),
+        formEmail3 = document.getElementById('form3-email'),
+        formTel3 = document.getElementById('form3-phone');
 
   const statusMessage = document.createElement('div');//добавялем элемент на страницу
   statusMessage.style.cssText = 'font-size: 2rem;';
   statusMessage.style.cssText = 'color: white;';
 
-  //проверка вводимых дынных-ТОЛЬКО ЦИФРЫ
-  formPhone.forEach((elem) =>{
-    elem.addEventListener('input', (e) =>{
-      let target = e.target;
-      if(target.matches('.form-phone')){
-        target.value = target.value.replace(/[^\+\d]/g, ''); // ограничиваем ввод всего кроме цифр
-      }
-    });
-  });
-
-
-  //проверка вводимых дынных-ТОЛЬКО кир и пробелы
-  formText.forEach((elem) =>{
+  input.forEach((elem) =>{
     topForm.classList.add('form-name');//добавили класс к input с классом top-form
-    elem.addEventListener('input', (e) =>{
+    mess.classList.add('form-name');//добавили класс к input с классом mess
+
+    elem.addEventListener('input', (e) =>{  //проверка вводимых дынных-ТОЛЬКО кир и пробелы в инпут ваше имя 
       let target = e.target;
       if(target.matches('.form-name')){
+        target.value = target.value.replace(/[^а-яё\s]/ig, ''); // ограничиваем ввод всего кроме цифр
+      }
+    });
+
+    elem.addEventListener('input', (e) =>{  //проверка вводимых дынных-ТОЛЬКО ЦИФРЫ в инпут телефон
+      let target = e.target;
+      if(target.matches('.form-phone')){
         target.value = target.value.replace(/[^\+\d]/g, ''); // ограничиваем ввод всего кроме цифр
       }
     });
@@ -427,6 +434,11 @@ const sendForm = () =>{
           statusMessage.textContent = errorMessage;
           console.error(error); 
     });
+
+    formName.value = '';
+    formEmail.value = '';
+    formTel.value = '';
+
   });
 
   form2.addEventListener('submit', (event) =>{
@@ -451,6 +463,11 @@ const sendForm = () =>{
           statusMessage.textContent = errorMessage;
           console.error(error); 
     });
+
+    formName2.value = '';
+    formEmail2.value = '';
+    formTel2.value = '';
+    mess.value = '';
   });
 
   form3.addEventListener('submit', (event) =>{
@@ -475,9 +492,14 @@ const sendForm = () =>{
           statusMessage.textContent = errorMessage;
           console.error(error); 
     });
+
+    formName3.value = '';
+    formEmail3.value = '';
+    formTel3.value = '';
   });
   
   const postData = (body, outputData, errorData) =>{
+
     //запрос к серверу
     const request = new XMLHttpRequest();// обьект, вызов функции конструктора
 
